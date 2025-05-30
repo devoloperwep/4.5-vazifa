d257c3b208e4dcce310281b2e7dece42f2e023f2
devoloperwep <wepdevoloper@gmail.com>
2025-05-30 14:48:21 +0500

    ,.

--- app.js
+++ app.js
@@ -779,3 +779,13 @@
 // } else {
 //   console.log(son);
 // }
+
+// if2. Butun son berilgan. Agar, berilgan son musbat bo`lsa, 1 ga oshiring, agar manfiy bo`lsa 2 ga kamaytiring. Agar 0 ga teng bo`lsa, 10 ni o`zlashtirsin. Hosil bo`lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// let son = 0;
+// if (0 < son) {
+//   console.log(son++);
+// } else if (0 > son) {
+//   console.log((son -= 2));
+// } else {
+//   console.log((son = 10));
+// }
